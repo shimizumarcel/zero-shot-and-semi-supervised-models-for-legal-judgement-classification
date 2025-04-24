934af64d52ab4a8f4be9da46eceeb0b1a87f6b62
Shimizu <156202924+shimizumarcel@users.noreply.github.com>
2025-04-24 18:22:22 -0300

    Update download_data.py

--- data/download_data.py
+++ data/download_data.py
@@ -2,14 +2,14 @@ import gdown
 import os
 
 FOLDER_ID = "1Et6VoWyHnN_F9E21-9xTU4ZSKJWYZqNS?usp=sharing"
-OUTPUT_DIR = os.path.join(os.getcwd(), "downloaded_data")
+OUTPUT_DIR = "./data/"
 
-def download_to_local():
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    gdown.download_folder(
-        url=f"https://drive.google.com/drive/folders/{FOLDER_ID}",
-        output=OUTPUT_DIR,
-        quiet,
-        use_cookies=False,
-        remaining_ok=True
-    )
+# Força o gdown a não criar subpasta
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+gdown.download_folder(
+    url=f"https://drive.google.com/drive/folders/{FOLDER_ID}",
+    output=OUTPUT_DIR,
+    quiet=False,
+    use_cookies=False,
+    remaining_ok=True  # Ignora arquivos que já existem
+)
