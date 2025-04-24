caae1fa5dbc582d5b5981d2a0adde18386aad647
Shimizu <156202924+shimizumarcel@users.noreply.github.com>
2025-04-24 18:14:29 -0300

    Update download_data.py

--- data/download_data.py
+++ data/download_data.py
@@ -1,22 +1,12 @@
-# download_data.py
 import gdown
 import os
 
 FOLDER_ID = "1Et6VoWyHnN_F9E21-9xTU4ZSKJWYZqNS?usp=sharing"
-FOLDER_URL = f"https://drive.google.com/drive/folders/{FOLDER_ID}"
-OUTPUT_DIR = "./data/"
+OUTPUT_DIR = os.path.join(os.getcwd(), "downloaded_data")
 
-def download_folder(folder_url, output_dir):
-    os.makedirs(output_dir, exist_ok=True)
-    
-
+def download_to_local():
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
     gdown.download_folder(
-        url=folder_url,
-        output=output_dir,
-        quiet=False,
-        use_cookies=False
-    )
-
-if __name__ == "__main__":
-    print("Downloading files...")
-    download_folder(FOLDER_URL, OUTPUT_DIR)
+        url=f"https://drive.google.com/drive/folders/{FOLDER_ID}",
+        output=OUTPUT_DIR,
+        quiet
